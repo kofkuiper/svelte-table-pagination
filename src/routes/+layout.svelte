62fea901832996eb8dc2f2bbcabaac56cfbfa572
kofkuiper <kofkuiper@gmail.com>
2023-03-25 23:58:01 +0700

    Improve demo page.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Footer from "$lib/components/Footer.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
+</script>
+
+<Navbar />
+
+<body class="antialiased" style="display: block;">
+  <div class="warpped">
+    <div class="page-wrapper">
+      <slot />
+      <Footer />
+    </div>
+  </div>
+</body>
