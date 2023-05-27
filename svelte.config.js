14c5990f982632a050444abc289bd89c303772bd
kofkuiper <kofkuiper@gmail.com>
2023-05-27 18:08:01 +0700

    Update config to using .svx file.

--- svelte.config.js
+++ svelte.config.js
@@ -11,46 +11,36 @@ const rehypePlugins = [
   [
     link_headings,
     {
-      behavior: `append`,
-      test: [`h2`, `h3`, `h4`, `h5`, `h6`], // don't auto-link <h1>
+      behavior: "append",
+      test: ["h2", "h3", "h4", "h5", "h6"], // don't auto-link <h1>
       content: s(
-        `svg`,
-        { width: 16, height: 16, viewBox: `0 0 16 16` },
+        "svg",
+        { width: 16, height: 16, viewBox: "0 0 16 16" },
         // symbol #octicon-link defined in app.html
-        s(`use`, { "xlink:href": `#octicon-link` })
+        s("use", { "xlink:href": "#octicon-link" })
       ),
     },
   ],
 ];
-const { default: pkg } = await import(`./package.json`, {
-  assert: { type: `json` },
-});
-const defaults = {
-  Wrapper: `svelte-zoo/CodeExample.svelte`,
-  pkg: pkg.name,
-  repo: pkg.repository,
-};
-const remarkPlugins = [[mdsvexamples, { defaults }]];
+const remarkPlugins = [
+  [mdsvexamples, { defaults: { Wrapper: "svelte-zoo/CodeExample.svelte" } }],
+];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [`.svelte`, `.svx`, `.md`],
+  extensions: [".svelte", ".svx", ".md"],
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: [
     vitePreprocess(),
-    mdsvex({ rehypePlugins, remarkPlugins, extensions: [`.svx`, `.md`] }),
+    mdsvex({ rehypePlugins, remarkPlugins, extensions: [".svx", ".md"] }),
   ],
 
   kit: {
     adapter: adapter(),
     alias: {
-      $site: `./src/site`,
+      $site: "./src/site",
     },
-  },
-  package: {
-    dir: "package",
-    emitTypes: true,
   },
 };
 
